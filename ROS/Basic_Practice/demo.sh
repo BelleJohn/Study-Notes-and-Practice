a582d2ff7369eeff67465aaa3985ad45a98efccf
BelleJohn <bellejohnru3mp6@gmail.com>
2025-11-13 18:12:29 +0100

    update demo.sh

--- ROS/Basic_Practice/demo.sh
+++ ROS/Basic_Practice/demo.sh
@@ -37,17 +37,6 @@ elif [ "$ARG1" == 'stop' ]; then
 angular:
   x: 0.0
   y: 0.0
-
-  z: 0.2"
-
-elif [ "$ARG1" == 'stop' ]; then
-    rostopic pub /cmd_vel geometry_msgs/Twist "linear:
-  x: 0.0
-  y: 0.0
-  z: 0.0
-angular:
-  x: 0.0
-  y: 0.0
   z: 0.0"
 
 else
